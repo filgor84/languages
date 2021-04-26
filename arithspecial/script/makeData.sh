cc755ad547b045a9f29af08d490bff8b9946181c
filgor84 <filgor84@gmail.com>
2021-04-26 20:02:47 +0200

    makeData improved

--- arithspecial/script/makeData.sh
+++ arithspecial/script/makeData.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-cd ~/go/src/github.com/filgor84/languages/arithspecial/data
 DIR="/dev/shm/data/"
 mkdir $DIR
 ### make 20 MB file
-cat 10MB.txt > "${DIR}20MB.txt"
+cat ../data/10MB.txt > "${DIR}20MB.txt"
 echo " + " >> "${DIR}20MB.txt"
-cat 10MB.txt >> "${DIR}20MB.txt"
+cat ../data/10MB.txt >> "${DIR}20MB.txt"
 ###make 100MB
-cat 10MB.txt > "${DIR}100MB.txt"
+cat ../data/10MB.txt > "${DIR}100MB.txt"
 for i in {1..9}
 do
 echo " + " >> "${DIR}100MB.txt"
-cat 10MB.txt >> "${DIR}100MB.txt"
+cat ../data/10MB.txt >> "${DIR}100MB.txt"
 done
 #make 1GB
-cat 10MB.txt >"${DIR}1GB.txt"
+cat ../data/10MB.txt >"${DIR}1GB.txt"
 for i in {1..99}
 do
 echo " + " >> "${DIR}1GB.txt"
-cat 10MB.txt >> "${DIR}1GB.txt"
+cat ../data/10MB.txt >> "${DIR}1GB.txt"
 done
 #make 10GB
-cat 10MB.txt >"${DIR}10GB.txt"
+cat ../data/10MB.txt >"${DIR}10GB.txt"
